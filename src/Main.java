98860f514797919fc8526bd183d4c38aac5f4862
ArtemYakomazov <tema.yakomazov@gmail.com>
2025-06-25 12:25:22 +0300

    Выполнил 5 задач урока №2 по условным операторам

--- src/Main.java
+++ src/Main.java
@@ -2,6 +2,68 @@
 // click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
 public class Main {
     public static void main(String[] args) {
-
+//Задача 1, 2
+int clientOS = 1;
+int clientDeviceYear = 2016;
+if (clientOS == 0) {
+    if (clientDeviceYear < 2015) {
+        System.out.println("Установите облегченную версию приложения для iOS по ссылке");
+    } else {
+        System.out.println("Установите версию приложения для iOS по ссылке");
     }
+} else if (clientOS == 1) {
+    if (clientDeviceYear < 2015) {
+        System.out.println("Установите облегченную версию приложения для Android по ссылке");
+    } else {
+        System.out.println("Установите версию приложения для Android по ссылке");
+    }
+}
+//Задача 3
+int year = 2020;
+if (year >= 1584) {
+    if ((year % 4 == 0 && year % 100 != 0) || (year % 400 == 0)) {
+        System.out.println(year + " год является високосным");
+} else {
+    System.out.println(year + " год не является високосным");
+    }
+}
+//Задача 4
+int deliveryDistance = 70;
+int deliveryDays = 1;
+if (deliveryDistance < 20) {
+    System.out.println("Потребуется дней " + deliveryDays);
+} else if (deliveryDistance >= 20 && deliveryDistance < 60) {
+    System.out.println("Потребуется дней " + deliveryDays * 2);
+} else if (deliveryDistance >= 60 && deliveryDistance <= 100) {
+    System.out.println("Потребуется дней " + deliveryDays * 3);
+} else {
+    System.out.println("Доставки нет");
+}
+//Задача 5
+int monthNumber = 6;
+switch (monthNumber) {
+    case 12:
+    case 1:
+    case 2:
+        System.out.println("Зима");
+        break;
+    case 3:
+    case 4:
+    case 5:
+        System.out.println("Весна");
+        break;
+    case 6:
+    case 7:
+    case 8:
+        System.out.println("Лето");
+        break;
+    case 9:
+    case 10:
+    case 11:
+        System.out.println("Осень");
+        break;
+    default:
+        System.out.println("Такого месяца нет");
+}
+     }
 }
